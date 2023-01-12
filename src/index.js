32c86c8a0177a2316520edec0439fe0e1361d061
Alexander <yusovsky2@gmail.com>
2023-01-12 15:15:45 +0200

    feature/modals-about&location

---
+++ src/index.js
@@ -0,0 +1,31 @@
+// opening modal "about"
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[about-modal-open]'),
+    closeModalBtn: document.querySelector('[about-modal-close]'),
+    modal: document.querySelector('[about-modal]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
+
+// opening modal "locations"
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[locations-modal-open]'),
+    closeModalBtn: document.querySelector('[locations-modal-close]'),
+    modal: document.querySelector('[locations-modal]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
